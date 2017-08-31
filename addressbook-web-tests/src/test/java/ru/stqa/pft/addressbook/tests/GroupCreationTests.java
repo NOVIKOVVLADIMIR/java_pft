84fac9270a71adb21e17fe6ed5c1d52bc0f3bbb3
NOVIKOVVLADIMIR <novikov_v79@mail.ru>
2017-08-31 14:06:07 +0300

    Создан GroupHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -10,10 +10,10 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
 
         app.gotoGroupPage();
-        app.initGroupCreation();
-        app.fillGroupForm(new GroupData("test11", "test11", "test11"));
-        app.submitGroupCreation();
-        app.returnToGroupPage();
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(new GroupData("test11", "test11", "test11"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
     }
 
 }
